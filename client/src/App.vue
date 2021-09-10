b547ac8519b69bcff2978b0de4e1e17b32b2125e
OnSebii <lang.sebastian.006@gmail.com>
2021-09-10 15:21:35 +0200

    Frage 1

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main class="blue">
+      <v-container yellow>
+        <h3 class="display-1">Hallo Vuetify Layout</h3>
+        <v-container red>
+          <h3 class="display-1">Hallo new container</h3>
+        </v-container>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
