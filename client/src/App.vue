a6202a5879f29b13d3578cd97409a246f77b0459
OnSebii <lang.sebastian.006@gmail.com>
2021-09-10 15:37:27 +0200

    Frage 2

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app>
     <v-main class="blue">
-      <v-container yellow>
-        <h3 class="display-1">Hallo Vuetify Layout</h3>
-        <v-container red>
-          <h3 class="display-1">Hallo new container</h3>
-        </v-container>
+      <v-container d-flex align-start flex-column yellow style="height: 500px">
+        <p class="mb-auto">Oben</p>
+        <v-spacer></v-spacer>
+        <p>Unten</p>
       </v-container>
     </v-main>
   </v-app>
